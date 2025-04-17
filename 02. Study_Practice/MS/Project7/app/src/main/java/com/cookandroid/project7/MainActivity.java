09a648befdea053bc5d33c70d6a85c8cdd56212b
한문섭 <hanmunseob@hanmunseob-ui-MacBookPro.local>
2025-04-17 14:31:13 +0900

    practice

---
+++ 02. Study_Practice/MS/Project7/app/src/main/java/com/cookandroid/project7/MainActivity.java
@@ -0,0 +1,39 @@
+package com.cookandroid.project7;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                final String[] versionArray = new String[] {"쫄면", "떡볶이", "김밥" };
+                final boolean[] checkArray = new boolean[] { true, false, false };
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("좋아하는 간식은?");
+                dlg.setIcon(R.mipmap.ic_launcher);
+                dlg.setMultiChoiceItems(versionArray, checkArray,
+                        new DialogInterface.OnMultiChoiceClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int which, boolean isChecked) {
+                                button1.setText(versionArray[which]);
+                            }
+                        });
+                dlg.setPositiveButton("닫기", null);
+                dlg.show();
+
+            }
+        });
+    }
+}
