e8ba5c9332f2dd503f5ae0c65aa4e59bcc3e9dfd
si-mong <ozmii333@gmail.com>
2025-05-08 13:59:56 +0900

    init

--- 01. Project_Source/Server/Server/index.js
+++ 01. Project_Source/Server/Server/index.js
@@ -1,10 +1,12 @@
 console.log('Happy developing ✨')
 // index.js
+// 주석입니다.
+// 안녕하세요.
 const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello from Node.js!');
+    res.end('Hello Eunjae!');
 });
 
 server.listen(3000, () => {
