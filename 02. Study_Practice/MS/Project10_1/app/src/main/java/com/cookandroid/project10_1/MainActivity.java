c8f4175f773152799ed5572e62028535ddd58e58
HanMoonSub <hanmoon3054@gmail.com>
2025-05-08 17:22:23 +0900

    init

--- 02. Study_Practice/MS/Project10_1/app/src/main/java/com/cookandroid/project10_1/MainActivity.java
+++ 02. Study_Practice/MS/Project10_1/app/src/main/java/com/cookandroid/project10_1/MainActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+//수정
 public class MainActivity extends Activity {
 
     @Override
