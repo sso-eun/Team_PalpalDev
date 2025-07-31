4bd3f8d860d064049fdb8b46760e62ac2b507444
sso-eun <soeun.office@gmail.com>
2025-08-01 04:43:10 +0900

    - 증명서 업로드/수정/전체조회/단일조회

--- 01. Project_Source/Server/Server/routes/uploadRouter.js
+++ 01. Project_Source/Server/Server/routes/uploadRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 // const upload = require('../controllers/multerConfig'); // multer 설정
-const { uploadProfile, uploadTalk } = require('../controllers/multerConfig');
+const { uploadProfile, uploadTalk,uploadCert } = require('../controllers/multerConfig');
 
 const uploadController = require('../controllers/uploadController');
 
 // POST /upload/profile/:user_num
 router.post('/profile/:user_num', uploadProfile.single('file'), uploadController.uploadProfileImage);
 router.post('/talk/:user_num', uploadTalk.single('file'), uploadController.uploadTalkImage);
+router.post('/cert/:user_num/:senior_num', uploadCert.single('file'), uploadController.uploadCertImage);
 
 module.exports = router;
