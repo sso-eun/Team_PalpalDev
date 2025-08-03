6bac1d4cc8dfb6c7d1ae2c5f6edfa65e222d90df
sso-eun <soeun.office@gmail.com>
2025-08-03 18:49:07 +0900

    talk_db_host 변경

--- 01. Project_Source/Server/Server/controllers/talkSendController.js
+++ 01. Project_Source/Server/Server/controllers/talkSendController.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2/promise");
 const db = mysql.createPool({
-    host: process.env.DB_LOCAL_HOST,
-    port: process.env.DB_LOCAL_PORT,
-    // host: process.env.DB_SERVER_HOST,
-    // port: process.env.DB_SERVER_PORT,
+    // host: process.env.DB_LOCAL_HOST,
+    // port: process.env.DB_LOCAL_PORT,
+    host: process.env.DB_SERVER_HOST,
+    port: process.env.DB_SERVER_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
