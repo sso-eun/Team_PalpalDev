a6d470da03809b452f89432735243ee526eef2c3
sso-eun <soeun.office@gmail.com>
2025-08-08 00:52:55 +0900

    Supabase - cert Image 업로드 기존 경로 동일하게 변경

--- 01. Project_Source/Server/Server/routes/uploadRouter.js
+++ 01. Project_Source/Server/Server/routes/uploadRouter.js
@@ -9,10 +9,10 @@ const resizeImage = require("../utils/resizeImage");
 // POST /upload/profile/:user_num
 router.post('/profile/:user_num', uploadProfile.single('file'), resizeImage, uploadController.supaUploadProfileImage);
 router.post('/talk/:user_num', uploadTalk.single('file'), resizeImage, uploadController.supaUploadTalkImage);
-router.post('/cert/:user_num/:senior_num', uploadCert.single('file'), resizeImage, uploadController.uploadCertImage);
-router.post('/supa-cert/:user_num/:senior_num', uploadSupa.single('file'), resizeImage, uploadController.uploadToSupabaseCert)
+router.post('/cert/:user_num/:senior_num', uploadSupa.single('file'), resizeImage, uploadController.uploadToSupabaseCert)
 
 // router.post('/cert/:user_num/:senior_num', uploadCert.single('file'), uploadController.uploadCertImage);
 // router.post('/upload-cert', uploadSupa.single('file'), resizeImage, uploadController.uploadToSupabaseCert)
 // router.post('/profile/:user_num', uploadProfile.single('file'), resizeImage, uploadController.uploadProfileImage);
+// router.post('/cert/:user_num/:senior_num', uploadCert.single('file'), resizeImage, uploadController.uploadCertImage);
 module.exports = router;
